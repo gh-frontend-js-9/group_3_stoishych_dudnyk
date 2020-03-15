2dec5055726dac7610a634286bba795ba7f0be46
Vitaliy5 <dydvitrom@ukr.net>
2020-03-15 14:59:58 +0200

    added footer component without links

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Home} from "./pages/Home";
 import {Header} from "./components/Header";
+import Footer from './components/Footer';
 
 function App() {
   return (
         <>
-          <Header/>
-          <Home/>
+          <Header />
+          <Home />
+          <Footer />
         </>
   );
 }
