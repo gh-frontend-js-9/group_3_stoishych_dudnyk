48ce3a5c1f9dd01ae4b8983c86fae2fd569d5bbb
Vitaliy5 <dydvitrom@ukr.net>
2020-03-15 16:42:40 +0200

    added font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import {Home} from "./pages/Home";
 import {Header} from "./components/Header";
 import Footer from './components/Footer';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { } from '@fortawesome/free-brands-svg-icons';
+import { } from '@fortawesome/free-solid-svg-icons';
+
+library.add()
+
 function App() {
   return (
         <>
