3839d58f34db9c79b514d2a6f3214202585da185
kristal5132 <stoishich99@gmail.com>
2020-03-15 12:57:23 +0200

    added responsive hero section with carousel and some fixes in header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import {Hero} from "../components/Hero/Hero";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export const Home = () => {
+    const sliderSettings = {
+        dots: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
     return (
         <main>
-            <Hero/>
+            <Slider {...sliderSettings}>
+                <Hero/>
+                <Hero/>
+                <Hero/>
+            </Slider>
         </main>
     )
 };
